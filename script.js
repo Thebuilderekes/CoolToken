c3b0eaf77eafb0ebdf3e8b51eb2eb0709de8f1c7
Ekeopre Beredugo <68848865+Thebuilderekes@users.noreply.github.com>
2021-12-22 12:34:28 +0000

    added media queries

---
+++ script.js
@@ -0,0 +1,10 @@
+const hamburgerMenu = document.getElementById("hamburger-menu");
+
+const navItems = document.getElementById("nav-items");
+
+function openNav(){
+navItems.style.display = "block";
+
+}
+
+
