cc894ac44821a7ad43050b9fa3648d8314cca5fd
Ekeopre Beredugo <68848865+Thebuilderekes@users.noreply.github.com>
2021-12-25 20:49:03 +0000

    updated navbar

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const hamburgerMenu = document.getElementById("hamburger-menu");
-
+const closeMenu = document.getElementById("close-menu");
 const navItems = document.getElementById("nav-items");
 
-function openNav(){
-navItems.style.display = "block";
+// window.addEventListener("mouseup", function(event){
+//  const navItems = document.getElementById("nav-items");
+//   if(event.target != navItems) {
 
+//     navItems.style.display = "none";
+//     closeMenu.style.display = "none";
+//     // hamburgerMenu.style.display = "block" 
+//   }
+
+// })
+function openNav(){
+navItems.style.display = "block" && "flex";
+hamburgerMenu.style.display =  "none";
+if (hamburgerMenu.style.display =  "none"){
+
+ closeMenu.style.display = "block";
+}
 }
 
+
+function closeNav(){
+navItems.style.display = "none";
+hamburgerMenu.style.display =  "block";
+
+if (hamburgerMenu.style.display =  "block"){
+
+ closeMenu.style.display = "none";
+}
+}
 
